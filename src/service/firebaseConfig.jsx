fa0777bd8e57692441c2bcde18ca71d450387554
Rohan <rathodrohan721@gmail.com>
2025-05-15 22:08:23 +0530

    Fix:Firebase Problem

--- src/service/firebaseConfig.jsx
+++ src/service/firebaseConfig.jsx
@@ -1,23 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBU2RIj144GF-hPa5SAWzV8hhieULsavIs",
-  authDomain: "rosy-hope-454305-a9.firebaseapp.com",
-  projectId: "rosy-hope-454305-a9",
-  storageBucket: "rosy-hope-454305-a9.firebasestorage.app",
-  messagingSenderId: "769479697806",
-  appId: "1:769479697806:web:c568f63b0fc9591da5b0df",
-  measurementId: "G-6274T344NW"
+  apiKey: "AIzaSyBK_ZEbIQApfBZ8H17NbXqQGurMpixgbEI",
+  authDomain: "ai-trip-planner-2f3c7.firebaseapp.com",
+  projectId: "ai-trip-planner-2f3c7",
+  storageBucket: "ai-trip-planner-2f3c7.firebasestorage.app",
+  messagingSenderId: "560151250381",
+  appId: "1:560151250381:web:b9c737cec903771e7bf084",
+  measurementId: "G-X6RW2E4K6M",
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app)
-// const analytics = getAnalytics(app);
+export const db = getFirestore(app);
